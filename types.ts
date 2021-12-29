285a1eeef08a4efdcd49f4621b2606526591beda
your-local-developer <73035294+your-local-developer@users.noreply.github.com>
2021-12-29 23:47:29 +0100

    Add missing documentation

--- types.ts
+++ types.ts
@@ -1,5 +1,8 @@
 /**
  * Object wich holds the dirname and filename infos
+ * 
+ * @since 0.1.0
+ * @author your-local-developer <https://github.com/your-local-developer>
  */
 export type FilePathInfo = {
   __dirname: string;
