a3b3726ed0cd6d3a728cd92ace25e51cc49ebf8f
your-local-developer <73035294+your-local-developer@users.noreply.github.com>
2021-12-31 13:59:16 +0100

    Update test.ts

--- test/test.ts
+++ test/test.ts
@@ -3,48 +3,36 @@ import { assertEquals, bold, cyan, underline } from "../test_deps.ts";
 
 Deno.test("dirname via fromMeta", () => {
   const { __dirname } = fromMeta(import.meta);
-  assertEquals(/\/test$/.test(__dirname), true);
-
   console.log(
     `
-
-    ${cyan(bold("Dirname"))}: ${underline(__dirname)} 
-    `,
+    ${cyan(bold("Dirname"))}: ${underline(__dirname)}`,
   );
+  assertEquals(/\/test$/.test(__dirname), true);
 });
 
 Deno.test("filename via fromMeta", () => {
   const { __filename } = fromMeta(import.meta);
-  assertEquals(/\/test\/test.ts$/.test(__filename), true);
-
   console.log(
     `
-    
-    ${cyan(bold("Filename"))}: ${__filename}
-    `,
+    ${cyan(bold("Filename"))}: ${__filename}`,
   );
+  assertEquals(/\/test\/test.ts$/.test(__filename), true);
 });
 
 Deno.test("dirname via getDirname", () => {
   const dirname = getDirname(import.meta);
-  assertEquals(/\/test$/.test(dirname), true);
-
   console.log(
     `
-  
-      ${cyan(bold("Dirname"))}: ${underline(dirname)} 
-      `,
+    ${cyan(bold("Dirname"))}: ${underline(dirname)}`,
   );
+  assertEquals(/\/test$/.test(dirname), true);
 });
 
 Deno.test("filename via getFilename", () => {
   const filename = getFilename(import.meta);
-  assertEquals(/\/test\/test.ts$/.test(filename), true);
-
   console.log(
     `
-
-    ${cyan(bold("Filename"))}: ${filename}
-    `,
+    ${cyan(bold("Filename"))}: ${filename}`,
   );
+  assertEquals(/\/test\/test.ts$/.test(filename), true);
 });
